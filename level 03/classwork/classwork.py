f4cea84f59ca60795fd75c9c8eeaeef62b06884f
Tekla1-tech <natianavrozashvili48@gmail.com>
2025-07-21 02:17:26 +0400

    commit

---
+++ level 03/classwork/classwork.py
@@ -0,0 +1,25 @@
+number=(7)
+number2=(1.2)
+
+print(number+ number2)
+
+name="tekla"
+surname="mchedlidze"
+print(name,surname)
+
+
+print(6+7)
+print(89+13)
+
+print(123-111)
+print(23-12)
+
+print(5*5)
+print(5*"7")
+
+
+print(25/5)
+print(12/7)
+
+print(30//5)
+print(120//20)
