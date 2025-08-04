c193ae86339179867b67bb4b37f2cfd221660498
Tekla1-tech <natianavrozashvili48@gmail.com>
2025-08-04 05:09:51 +0400

    second commit

---
+++ level 05/classwork/classwork.py
@@ -0,0 +1,28 @@
+# debugging- შეცდომის გამოსწორება კოდსში,რათა მან სწორად იმუშაოს
+# case-sensetive -განსხვავდება დიდი და პატარა ასოები.
+# pyrhon case sensetive ენაა რადგან ის განასხვავებს დიდ და პატარა ასოებს.
+
+num_1=7
+name_1="tekla"
+fav_movie="avatar"
+my_age=16
+item_07="computer"
+print(num_1)
+print(name_1)
+print(fav_movie)
+print(my_age)
+print(item_07)
+
+#my name="tekla" ცვლადში არის ცარიელი ადგილი ( gap between myname)
+#age!=16 ცვლადში არის სიმბოლო
+#4friend="love" ცვლადი იწყება რიცხვით
+#73stream="goa" ცვლადი რიცხვით იწყება
+#movie 2="notebook" ცვლადებს შორის სიცარიელეა (gap)
+
+a=7
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
---
+++ level 05/homework/homework.py
@@ -0,0 +1,67 @@
+# davaleba2
+#კომენტარს python-ში ბევრი დანიშნულება აქვს,მაგრამ მისი ერთ-ერთი დანიშნულება კოდის ახსნაა.
+# კომენტარის საშუალებით ადამიანს ეძლევა საშუალება მარტივად აღიქვას და გაიაზროს კოდი.
+ 
+# davaleba3
+dog_name="chika"
+fav_song="let down"
+fav_artist="ozzy osbuorne"
+last_role="foreste"
+my_age=16
+fav_num=5
+game_1="sudoku"
+name_4="tekla"
+cr_year=2025
+cr_working="python"
+
+print(dog_name)
+print(fav_song)
+print(fav_artist)
+print(last_role)
+print(my_age)
+print(fav_num)
+print(game_1)
+print(name_4)
+print(cr_year)
+print(cr_working)
+
+# davaleba4
+name1 = "Luka"
+age_num = 56
+my_favmov = "blood and bone"
+my_friends_name= "Nika"
+print("My name is" + name1)
+print("my age is" + "age_num")
+print("My Favorite Movie is" + my_favmov)
+print("My Friend's name is" + my_friends_name)
+
+
+# davaleba5
+a="tekla"
+b="mchedlidze"
+c=a+b
+print(c)
+
+z="blood and"
+t="bone"
+f=z+t
+print(f)
+
+word_1="liberty"
+fav_place="theatre"
+combained=word_1+fav_place
+print(combained)
+
+cold="ice"
+kind_needs="queen"
+together=cold+kind_needs
+print(together)
+
+star1="sun"
+plant="flower"
+A=star1+plant
+print(A)
+
+# davaleba 6
+
+# snake-case-ცვლადის შექმნის გზა,სიტყვები ერთმანეთისგან დაშორებულია ქვედატირით.
