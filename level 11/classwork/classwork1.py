1994c1a29f0e4ede00c640bc5c499b726528aa3e
Tekla1-tech <natianavrozashvili48@gmail.com>
2025-09-21 07:16:15 +0400

    second commit:

---
+++ level 11/classwork/classwork1.py
@@ -0,0 +1,15 @@
+# დავალება 5
+for i in range(20):
+    print("tekla")
+
+#დავალება 6
+for i in range(1,10):
+    print(i)
+
+#დავალება 7
+for i in range(1,50,2):
+    print(i)
+
+#დავალება 8
+for i in range(0,67,2):
+    print(i)
