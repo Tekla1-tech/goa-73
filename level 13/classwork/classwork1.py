44cb19a5e1b4965d83b8e4cb214a17356fd98c0e
Tekla1-tech <natianavrozashvili48@gmail.com>
2025-10-01 04:47:47 +0400

    second commit

---
+++ level 13/classwork/classwork1.py
@@ -0,0 +1,29 @@
+#დავალება 5
+for i in range(0,50,2):
+    print(i)
+
+#დავალება 6
+
+for i in range(10):
+    print("tekla")
+
+#დავალება 7
+
+i=1
+
+while i<67:
+    print(i)
+    i=i+1
+
+#დავალება 8
+i=0
+while i<10:
+    print("tekla")
+    i=i+1
+
+#დავალება 9
+num1=17
+user_3=int(input("enter your number"))
+while num1<user_3:
+    print("hello world")
+    
